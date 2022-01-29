2713784e98fccdcf95c40ac6f225b8b9200f3cba
PhilosophyWithJosh <joshmendiola@icloud.com>
2022-01-28 21:13:13 -0600

    added the funcitonality for the new listener

--- besosMC/src/me/philosophywithjosh/besosMC/Main.java
+++ besosMC/src/me/philosophywithjosh/besosMC/Main.java
@@ -2,6 +2,7 @@ package me.philosophywithjosh.besosMC;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import me.philosophywithjosh.besosMC.listeners.crouchListener;
+import me.philosophywithjosh.besosMC.listeners.playerListener;
 
 public class Main extends JavaPlugin
 {
@@ -9,5 +10,6 @@ public class Main extends JavaPlugin
 	public void onEnable()
 	{
 		new crouchListener(this);
+		new playerListener(this);
 	}
 }
